d0da9415d1cfdd6b9e2c4b0b74392a07e8aed0e0
M Abdullah <105612170+Abdullahofficial00@users.noreply.github.com>
2023-09-30 01:30:32 +0500

    Add: Implementing Servies for Account

--- Moody Foody/lib/main.dart
+++ Moody Foody/lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:food/login.dart';
+import 'signup.dart';
+import 'home.dart';
 
-void main()=> runApp(MaterialApp(
-  title: "Foody Moody",
-  home: Center(
-    child: Text("Foody Moody",
-    textAlign: TextAlign.center,
-    style: TextStyle(fontSize: 10, fontWeight:FontWeight.bold,),
-    
-    ),
-    
-  ),
-  
-  debugShowCheckedModeBanner: false, 
-  
-  
-));
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Main',
+      routes: {
+        '/login': (context) => LoginPage(), // Correct the class name
+        '/sign_up': (context) => SignUp(),
+        '/home': (context) => Home(),
+      },
+      home: Home(), // Set the LoginScreen as the initial screen
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
