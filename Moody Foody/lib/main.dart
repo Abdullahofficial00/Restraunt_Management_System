f082827c172b38acdfd4ec2a891e093e5b841739
M Abdullah <105612170+Abdullahofficial00@users.noreply.github.com>
2023-10-01 04:29:24 +0500

    Add Text

---
+++ Moody Foody/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main()=> runApp(MaterialApp(
+  title: "Foody Moody",
+  home: Center(
+    child: Text("Foody Moody",
+    textAlign: TextAlign.center,
+    style: TextStyle(fontSize: 10, fontWeight:FontWeight.bold,),
+    
+    ),
+    
+  ),
+  
+  debugShowCheckedModeBanner: false, 
+  
+  
+));
